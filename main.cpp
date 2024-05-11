927b9bfe8a3a6d4a33679694e7e953237de4fd4d
Mizakson <maxwellizakson@gmail.com>
2024-05-11 10:46:12 -0400

    Create double square, print_square funcs

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 
 #include <iostream>
 
+using namespace std;
+
+double square(double x)
+{
+    return x * x;
+}
+
+void print_square(double x)
+{
+    cout << "the square of "<< x <<" is " << square(x) << "\n";
+}
+
 int main() 
 {
-    std::cout << "Hello World! \n";
+    print_square(2);
+    // cout << "Hello World! \n";
 }
+
